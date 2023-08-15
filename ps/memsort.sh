e20c6f5de336d96406e0a0a00f4536b1f2cb56d6
GitMarvel <infocontactin0@gmail.com>
2023-08-15 07:33:07 +0530

    mem

--- ps/memsort.sh
+++ ps/memsort.sh
@@ -2,4 +2,4 @@
 #Sort by memory,cpu and show pid,ppid,cmd along with hostname
 hostname
 echo
-ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem --sort=-%cpu
+ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem|tail -20
